d6a1f5f0ae072115246100e842a0f204bbdc9c62
Kate-Pikachu <pikulina.ev@phystech.edu>
2021-02-17 20:29:00 +0300

    task 2

--- main.cpp
+++ main.cpp
@@ -1,23 +1,48 @@
 #include<iostream>
+#include<algorithm>
+#include<chrono>
 #include<vector>
+#include<array>
+#include<deque>
+#include<list>
+#include<forward_list>
+
+
+using namespace std;
+using namespace chrono;
+
+template <typename T>
+void sort_time(T x) {
+	auto begin = steady_clock::now();
+	sort(x.begin(), x.end());
+	auto end = steady_clock::now();
+	auto delta = duration_cast<nanoseconds>(end - begin);
+	cout << "Time: " << delta.count() << " nanoseconds - ";
+}
+
+template <typename P>
+void sort_time_listiki(P x) {
+	auto begin = steady_clock::now();
+	x.sort();
+	auto end = steady_clock::now();
+	auto delta = duration_cast<nanoseconds>(end - begin);
+	cout << "Time: " << delta.count() << " nanoseconds - ";
+}
+
 
 int main() {
-	using namespace std;
 
-	long int n = 1000000000;
-	vector<int> vec(n);
+	vector<int> vec = { 1,3,5,7,9 };
+	array<int, 5> arr = { 1,3,5,7,9 };
+	deque<int> dq = { 1,3,5,7,9 };
+	list<int> ls = { 1,3,5,7,9 };
+	forward_list<int> fl = { 1,3,5,7,9 };
 
-	cout << "Size: " << vec.size() << endl;
-	cout << "Capacity: " << vec.capacity() << endl;
-	for (auto i = 0; i < n; i++) {
-		vec[i] = 12;
-	}
-	vec.push_back(11); //добавили в вектор 1 новый элемент - размер вектора: 1000000001, объем: ?
-
-	cout << "Size: " << vec.size() << endl;
-	cout << "Capacity: " << vec.capacity() << endl;
-	//наблюдени€: если вводить слишком большой размер вектора, то компьютер очень греетс€ и пыхтит во врем€ компил€ции,
-	//ну и в результате он не т€нет и выдает ошибку
+	sort_time(vec); cout << "vector" << endl;
+	sort_time(arr); cout << "array" << endl;
+	sort_time(dq); cout << "deque" << endl;
+	sort_time_listiki(ls); cout << "list" << endl;
+	sort_time_listiki(fl); cout << "forward list" << endl;
 
 	return EXIT_SUCCESS;
 }

0c4595182b975af2ed383a03a218fb81fdc3160e
Kate-Pikachu <pikulina.ev@phystech.edu>
2021-02-17 19:42:35 +0300

    3 park of 1 task

--- main.cpp
+++ main.cpp
@@ -4,17 +4,20 @@
 int main() {
 	using namespace std;
 
-	vector<int> vec = { 1, 3, 5, 7, 9 }; // размер вектора: 5;
-	vec.reserve(6); // зарезервировали объем: 6
-	cout << "Size: " << vec.size() << endl;
-	cout << "Capacity: " << vec.capacity() << endl;
-
-	vec.push_back(2); 
-	vec.push_back(4); //добавили в вектор 2 новых элемента - размер вектора: 7, объем: ?
+	long int n = 1000000000;
+	vector<int> vec(n);
 
 	cout << "Size: " << vec.size() << endl;
 	cout << "Capacity: " << vec.capacity() << endl;
-	// в выводе можно увидеть, что при нехватке пам€ти количество €чеек вектора стало 9
+	for (auto i = 0; i < n; i++) {
+		vec[i] = 12;
+	}
+	vec.push_back(11); //добавили в вектор 1 новый элемент - размер вектора: 1000000001, объем: ?
+
+	cout << "Size: " << vec.size() << endl;
+	cout << "Capacity: " << vec.capacity() << endl;
+	//наблюдени€: если вводить слишком большой размер вектора, то компьютер очень греетс€ и пыхтит во врем€ компил€ции,
+	//ну и в результате он не т€нет и выдает ошибку
 
 	return EXIT_SUCCESS;
 }
